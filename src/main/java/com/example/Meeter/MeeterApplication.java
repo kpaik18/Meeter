e8cb38e7d5340b2e0a3c743ab69df53c3a3d6721
Kobbit <kpaik18@freeuni.edu.ge>
2023-08-06 18:04:56 +0400

    first commit

---
+++ src/main/java/com/example/Meeter/MeeterApplication.java
@@ -0,0 +1,13 @@
+package com.example.Meeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Meeter/MeeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Meeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
